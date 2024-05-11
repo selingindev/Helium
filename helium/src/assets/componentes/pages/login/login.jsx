e4d62d1f9f19a8fded6f1500ca299c34c1a37347
Arthur Selingin <arthur_santos@outlook.com.br>
2024-05-11 14:44:30 -0300

    ajustando

---
+++ helium/src/assets/componentes/pages/login/login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {}
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar sua lógica de autenticação
+    if (username === 'usuario' && password === 'senha') {
+      setLoggedIn(true); // Login bem-sucedido
+    } else {
+      alert('Login falhou. Verifique seu usuário e senha.');
+    }
+  };
+
+  return (
+    <div>
+      {!loggedIn ? (
+        
+        <form className='loginForm'>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      ) : (
+        <div>
+          <p>Você está logado!</p>
+          {/* Aqui você pode redirecionar o usuário para outra página ou renderizar outro componente */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from './assets/componentes/pages/login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
